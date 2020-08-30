5ec6cd94ca2e84c9d295b08ea6a0b3fdc5b569b0
mateen <admatinm@gmail.com>
2020-08-30 23:05:19 +0600

    build compose-message

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Toolbar from "./toolbar/Toolbar";
+import ComposeMessage from "./compose-message/ComposeMessage";
+import AllMessages from "./AllMessages/AllMessages";
 
-const MessageList = () => <Toolbar />;
+const MessageList = () => (
+  <>
+    <Toolbar />
+    <AllMessages />
+    <ComposeMessage />
+  </>
+);
 
 export default MessageList;
