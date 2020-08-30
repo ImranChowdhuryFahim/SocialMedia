4dc3e60ae3012aa440c945fc1624c86c7843045e
Imran <u1704107@student.cuet.ac.bd>
2020-08-30 14:31:43 +0600

    Scrollable Chatheads v3

--- src/components/ChatHead.js
+++ src/components/ChatHead.js
@@ -1,7 +1,6 @@
 import React,{ Component } from 'react'
 import styles from '../styles/ChatHead.module.css'
 import Banner from './ChatHeadSubComponents/Banner'
-import SearchBar from './ChatHeadSubComponents/SearchBar'
 import Contacts from './ChatHeadSubComponents/Contacts'
 
 class ChatHead extends Component{
@@ -10,8 +9,6 @@ class ChatHead extends Component{
         return(
             <div className={styles.ChatHead}>
                 <Banner></Banner>
-
-                <SearchBar></SearchBar>
 
                 <Contacts></Contacts>
                 
--- src/components/ChatHeadSubComponents/Contacts.js
+++ src/components/ChatHeadSubComponents/Contacts.js
@@ -1,6 +1,7 @@
 import React,{ Component } from 'react'
 import styles from './styles/Contacts.module.css'
 import ChatHeadDiv from './ChatHeadDiv'
+import SearchBar from './SearchBar'
 
 class Contacts extends Component{
     render()
@@ -10,6 +11,7 @@ class Contacts extends Component{
                 {/* <div className={styles.Label}>
                     Contacts
                 </div> */}
+                <SearchBar></SearchBar>
                 <ChatHeadDiv></ChatHeadDiv>
                 <ChatHeadDiv></ChatHeadDiv>
                 <ChatHeadDiv></ChatHeadDiv>
